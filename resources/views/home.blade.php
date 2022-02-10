844894f20eeb4b5d98440c6828b08d0cdf544678
Alessio2021 <alessiobut1911@gmail.com>
2022-02-10 14:26:51 +0100

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-primi-passi</title>
+</head>
+<body>
+    <header>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/shop">Shop</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/forum">Forum</a></li>
+        </ul>
+    </header>
+
+    <h1>Hello World</h1>
+    <h2>{{$name}} {{$lastname}}</h2>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+
+    $data = [
+        'name' => 'Alessio',
+        'lastname' => 'Pippo',
+    ];
+
+    return view('home',  $data);
+});
+
+Route::get('/shop', function () {
+    return view('shop');
+});
+
+Route::get('/contact', function () {
+
+    $contacts = [
+        'name' => ['pippo', 'pluto', 'paperino', 'paperina']
+    ];
+    return view('contact', $contacts);
+});
+
+Route::get('/forum', function () {
+    return view('forum');
+});
+
